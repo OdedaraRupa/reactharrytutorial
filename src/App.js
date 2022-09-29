581b87db2b945b8706c7347fd83f525ae085940a
Rupa Odedara <odedararupa35@gmail.com>
2022-09-29 15:13:29 +0530

    Enable Dark and light mode, Added all controles of dark and light mode in App.js, Changed styling and design of some components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 // import About from './components/About';
+import React,{ useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode,setMode] = useState('light')
+  const toogleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = "#000"
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = "#fff"
+    }
+  }
+  
   return (
     
     <>
-      <Navbar title="TextUtils"></Navbar>
-      <div className="container mb-3">
-          <TextForm heading="Enter the text to analyze below "></TextForm>
-          {/* <About /> */}
-      </div>
+      <Navbar title="TextUtils" mode={mode} toggleMode= {toogleMode} ></Navbar>
+      <TextForm heading="Enter the text to analyze below " mode={mode}></TextForm>
+      {/* <About /> */}
     </>
 
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,23 +29,22 @@ export default function TextForm(props) {
 
   return (
     <>
+      <div className="container mb-3" style={{color: props.mode === 'light'?"#212529":"#fff"}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleChange} id="box" rows="8" style={{backgroundColor: props.mode === 'dark'?"#212529":"#fff",color: props.mode === 'light'?"#212529":"#fff"}}></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleupClick} > Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleloClick} > Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClear} > Clear All</button>
+        <button className="btn btn-primary mx-2" onClick={handleSelectText} > Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={removeExtraSpace} > Remove Extra pace</button>
 
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleChange} id="box" rows="8"></textarea>
+        <h2 className="my-4">Your Text Summary</h2>
+        <h3 className="my-4">{text.split(" ").length} Words {text.length} Chracters</h3>
+        <h4>{0.008 * text.split(" ").length} Minutes Read</h4>
+        <p>{text}</p>
       </div>
-      <button className="btn btn-primary mx-2" onClick={handleupClick} > Convert to UpperCase</button>
-      <button className="btn btn-primary mx-2" onClick={handleloClick} > Convert to LowerCase</button>
-      <button className="btn btn-primary mx-2" onClick={handleClear} > Clear All</button>
-      <button className="btn btn-primary mx-2" onClick={handleSelectText} > Copy Text</button>
-      <button className="btn btn-primary mx-2" onClick={removeExtraSpace} > Remove Extra pace</button>
-
-      
-      
-      <h2 className="my-4">Your Text Summary</h2>
-      <h3 className="my-4">{text.split(" ").length} Words {text.length} Chracters</h3>
-      <h4>{0.008 * text.split(" ").length} Minutes Read</h4>
-      <p>{text}</p>
     </>
   )
 }
